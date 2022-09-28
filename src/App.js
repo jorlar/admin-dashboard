524846ab9ebd22f79970f206c5356fb8c4ed5701
Jørn Larsen <jorn.larsen@jotun-utvikling.no>
2022-09-28 12:22:49 +0200

    :sparkles: Add features and finilize Ecomerce

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const App = () => {
                 }>
                     <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
                         <Navbar />
-                    </div>
+                    
                 </div>
                 <div>
                     <Routes>
@@ -69,6 +69,7 @@ const App = () => {
                         <Route path="/pyramid" element={<Pyramid/>} />
                         <Route path="/stacked" element={<Stacked/>} />
                     </Routes>
+                </div>
                 </div>
             </div>
         </BrowserRouter>
